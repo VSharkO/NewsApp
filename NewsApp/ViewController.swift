8ab6a8784acf95a9ccb26c46b467be751c25a2ab
Valentin Šarić <valentin.saric1@gmail.com>
2018-10-22 12:31:58 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Valentin Šarić on 22/10/2018.
+//  Copyright © 2018 Valentin Šarić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
