1b89bfc37f150bf100d7d40c7f6b1c65c249cb63
Valentin Šarić <valentin.saric1@gmail.com>
2018-10-25 11:29:16 +0200

    da

--- NewsApp/MainScreen/Presenter/MainPresenterImpl.swift
+++ NewsApp/MainScreen/Presenter/MainPresenterImpl.swift
@@ -6,38 +6,31 @@
 //  Copyright © 2018 Valentin Šarić. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 class MainPresenterImpl : MainPresenter{
 
-    var fakeRepository = FakeRepository()
-    var data : [Article]? = []
+    var articleRepository = ArticleRepository()
+    var data : [Article] = []
     weak var view : MainView?
     var numberOfLoadedImages = 0
     
     init(view : MainView) {
         self.view = view
         getDataFromRepository()
-        
     }
     
-    func getSingleNews(index: Int) -> Article? {
-        if let articles = data?[index]{
-            return articles
-        }else{
-            getDataFromRepository()
-            return nil
-        }
+    func getNews() -> [Article] {
+        return data
     }
     
-    func getPictureFromRepository(forArticleAtIndex: Int, response: @escaping (Bool,UIImage?,Error?) -> Void){
-        if let articles = data{
-            fakeRepository.getPictureFromUrl(url: articles[forArticleAtIndex].urlToImage) { [weak self](success,data,error) in
+    func getPictureFromRepository(forArticleAtIndex: Int, response: @escaping (Bool,Data?,Error?) -> Void){
+            articleRepository.getPictureFromUrl(url: data[forArticleAtIndex].urlToImage) { [weak self](success,data,error) in
                 guard let strongSelf = self else{
                     return
                 }
                 if(success){
-                    if let anyAsData = UIImage(data: data as! Data){ //ovaj UIImage? ok ili da bude data pa da view datu prebaci u UIImage?
+                    if let anyAsData = data as? Data{ //ovaj UIImage? ok ili da bude data pa da view datu prebaci u UIImage?
                         response(true,anyAsData,nil)
                         
                         if(!strongSelf.checkIfAllImagesAreLoaded()){
@@ -51,44 +44,30 @@ class MainPresenterImpl : MainPresenter{
                     strongSelf.view?.hideSpinner()
                 }
             }
-        }
     }
     
     func checkIfAllImagesAreLoaded() -> Bool{
-        if let dataCount = data?.count{
-            if numberOfLoadedImages == dataCount-1{
+        if numberOfLoadedImages == data.count-1{
                 return true
             }else{
                 return false
             }
-        }
-        return false
     }
     
     func getDataFromRepository(){
         view?.showSpinner()
-        fakeRepository.getResponseFromUrl { [weak self](success, arrayOfArticles, error) in
+        articleRepository.getResponseFromUrl { [weak self](success, arrayOfArticles, error) in
             guard let strongSelf = self else{
                 return
             }
             if(success){
                 if let articles = arrayOfArticles{
                     strongSelf.data = articles
-                    FakeDatabase.database = articles
                 }
                 strongSelf.view?.reloadData()
             }
         }
     }
-    
-    func getNumberOfItems() -> Int {
-        if let count = data?.count{
-            return count
-        }else{
-            return 0
-        }
-    }
-    
 }
 
 
